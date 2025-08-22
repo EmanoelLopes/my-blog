582d908fc6309d90d31e6089332af27d68eef58a
emanoellopes <emanoel.lopes.web@gmail.com>
2025-08-22 15:07:57 -0300

    chore: recreating structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,29 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import partytown from '@astrojs/partytown';
+
+import tailwindcss from '@tailwindcss/vite';
+
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://www.fx.dev.br',
+
+  integrations: [
+    partytown(),
+  ],
+
+  markdown: {
+    shikiConfig: {
+      theme: 'catppuccin-macchiato',
+    },
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  adapter: netlify(),
+});

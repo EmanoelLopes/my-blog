3b5b3fffecd59b7de74d8646327d69d693995cb9
emanoellopes <emanoel.lopes.web@gmail.com>
2025-08-22 17:48:31 -0300

    chore: structure development - wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import partytown from '@astrojs/partytown';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import netlify from '@astrojs/netlify';
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://www.fx.dev.br',
+  site: 'https://emanoellopes.dev',
 
   integrations: [
     partytown(),

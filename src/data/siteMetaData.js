dfecf268a0f704f0f3178530034592d9810cf6ab
emanoellopes <emanoel.lopes.web@gmail.com>
2025-08-23 04:56:12 -0300

    chore: site metadata

---
+++ src/data/siteMetaData.js
@@ -0,0 +1,13 @@
+export const site = {
+  title: 'Emanoel Lopes | emanoellopes.dev',
+  metaTags: {
+    og: {
+      title: 'Emanoel Lopes | emanoellopes.dev',
+      type: 'website',
+      url: 'https://emanoellopes.dev',
+      description: 'Emanoel Lopes | Meu site pessoal onde de vez em quando escrevo sobre assuntos técnicos relacionados ao mundo do desenvolvimento frontend.',
+      image: 'https://avatars.githubusercontent.com/u/4943185?v=4',
+      imageAlt: 'Imagem de Emanoel Lopes',
+    },
+  },
+};
